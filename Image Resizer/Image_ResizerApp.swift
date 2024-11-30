aa12c7a8d8bd22605eba72b4246e3cda6bdc804d
dankamel <danielufkamel@gmail.com>
2024-11-30 17:06:30 +0000

    Initial Commit

---
+++ Image Resizer/Image_ResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ResizerApp.swift
+//  Image Resizer
+//
+//  Created by Daniel Kamel on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Image_ResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
